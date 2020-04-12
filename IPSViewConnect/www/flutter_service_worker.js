841ab7d99ac38cbe0902fc0456afe8a761bd5690
Andreas Brauneis <andreas.brauneis@gmx.at>
2020-04-12 16:58:10 +0200

    New Client Version

---
+++ IPSViewConnect/www/flutter_service_worker.js
@@ -0,0 +1,58 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "assets/AssetManifest.json": "ce062bb1a50c19659803e54bf78003bc",
+"assets/FontManifest.json": "1f14fc381e9565c7a2462433254f69db",
+"assets/fonts/DancingScript-Bold.ttf": "e4daab0ef72e9ec839c2962a5b6c10d3",
+"assets/fonts/DancingScript-Regular.ttf": "b464889840d2a96a5dc30059d9f4ad9a",
+"assets/fonts/IndieFlower-Regular.ttf": "0841af952c807bdf56455b1addb4c7df",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/fonts/OpenSans-Bold.ttf": "1025a6e0fb0fa86f17f57cc82a6b9756",
+"assets/fonts/OpenSans-BoldItalic.ttf": "3a8113737b373d5bccd6f71d91408d16",
+"assets/fonts/OpenSans-Regular.ttf": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+"assets/fonts/OpenSans-RegularItalic.ttf": "f6238deb7f40a7a03134c11fb63ad387",
+"assets/fonts/PTSans-Bold.ttf": "333ee0ee5989e593812c23ca2dd7bc24",
+"assets/fonts/PTSans-BoldItalic.ttf": "22f2e7f9ae109154c0467619164247ea",
+"assets/fonts/PTSans-Regular.ttf": "4ea26cd5e7f64894d6c2451446f7dda5",
+"assets/fonts/PTSans-RegularItalic.ttf": "a97ccf1e30117c053dd28f265c270a22",
+"assets/fonts/RobotoMono-Bold.ttf": "7c13b04382bb3c4a6a50211300a1b072",
+"assets/fonts/RobotoMono-BoldItalic.ttf": "4a0b78a48050f97c16ef6fc518afd362",
+"assets/fonts/RobotoMono-Regular.ttf": "b4618f1f7f4cee0ac09873fcc5a966f9",
+"assets/fonts/RobotoMono-RegularItalic.ttf": "c37c35a80051edc42d141ec301066052",
+"assets/graphics/ImageNotFound_128.png": "9195566839639503a6d131ad4bb5abb9",
+"assets/graphics/IPSView_512_r10.png": "8b08668a235d8d8e30cbb6a45ea62323",
+"assets/LICENSE": "990f661ba6ce9a3bc406ad5c7b2c9a1d",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
+"assets/packages/timezone/data/2019a.tzf": "32dd977d022c6c600d5fab20e326277d",
+"favicon.png": "1a6fa949923ad28135a9cc5fb9ef3590",
+"icons/Icon-192.png": "b413209fbdaea61a849db356aa122aef",
+"icons/Icon-512.png": "40caace896cb8cbdad146a96d1843fa6",
+"index.html": "b84cf65f2cd29c5ff5e65a028d87e3e2",
+"/": "b84cf65f2cd29c5ff5e65a028d87e3e2",
+"main.dart.js": "260c622f439ab3861cca52f33b3699fb",
+"manifest.json": "338f6f11d77d2125e21a6aeafe1e7822"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
