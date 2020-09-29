4c441c7a64d1f4dc9ac458a76a27944c29e7fa6e
Andreas Brauneis <andreas.brauneis@gmx.at>
2020-09-29 21:08:50 +0200

    New WebClient 5.0.22

--- IPSViewConnect/www/flutter_service_worker.js
+++ IPSViewConnect/www/flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {
 "icons/spinner.svg": "a3d42232c10a0e3446f94d081d5f213c",
 "index.html": "5c46e12327b5df6f4a0a65eac8c7c38d",
 "/": "5c46e12327b5df6f4a0a65eac8c7c38d",
-"main.dart.js": "d5313fe6bd1ff0ab85e812b5eef58458",
+"main.dart.js": "2d460a938b868d6de2abf35d24e40ef6",
 "manifest.json": "338f6f11d77d2125e21a6aeafe1e7822",
 "webfront.html": "df1e34767cd31d5534b226b059ec30a7"
 };
