342692ffe8847c36c49ae4640e90d82bf36476a8
ab <support@brownson.at>
2023-08-15 10:19:05 +0200

    WebClient v6.2.1

--- IPSViewConnect/www/flutter_service_worker.js
+++ IPSViewConnect/www/flutter_service_worker.js
@@ -12,7 +12,7 @@ const RESOURCES = {"assets/AssetManifest.json": "46a206b4a04d0f2e7a2df70c8f812a6
 "assets/fonts/Roboto-Regular.ttf": "11eabca2251325cfc5589c9c6fb57b46",
 "assets/fonts/Roboto-RegularItalic.ttf": "a720f17aa773e493a7ebf8b08459e66c",
 "assets/graphics/IPSView_512_r10.png": "8b08668a235d8d8e30cbb6a45ea62323",
-"assets/NOTICES": "d948a0eb0b33de730f049b5ac6b800de",
+"assets/NOTICES": "87be954195072ed6603be1828317ea71",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/packages/flutter_inappwebview/assets/t_rex_runner/t-rex.css": "5a8d0222407e388155d7d1395a75d5b9",
 "assets/packages/flutter_inappwebview/assets/t_rex_runner/t-rex.html": "16911fcc170c8af1c5457940bd0bf055",
@@ -55,11 +55,11 @@ const RESOURCES = {"assets/AssetManifest.json": "46a206b4a04d0f2e7a2df70c8f812a6
 "icons/Icon-192.png": "b413209fbdaea61a849db356aa122aef",
 "icons/Icon-512.png": "40caace896cb8cbdad146a96d1843fa6",
 "icons/spinner.svg": "a3d42232c10a0e3446f94d081d5f213c",
-"index.html": "d355d3c5e0582c52fc3f14528a0cf34e",
-"/": "d355d3c5e0582c52fc3f14528a0cf34e",
-"main.dart.js": "b449d81e2aae94135604c505b3dc3c6b",
+"index.html": "11c89478e39582708f5e55e024fe1028",
+"/": "11c89478e39582708f5e55e024fe1028",
+"main.dart.js": "775a4d88f04f71a9b5c9db666d1d72b0",
 "manifest.json": "62173ee6a7a5ef9b24a3145e2de6de80",
-"version.json": "7f66316fc0da84115eda90bd97d2e5b4",
+"version.json": "f40256edaf42d605a736c42f374582d4",
 "webfront.html": "4273eca033430ee384153b11aca49764"};
 // The application shell files that are downloaded before a service worker can
 // start.
