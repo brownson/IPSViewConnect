447d0368686640818938514710574ab6ebd478f3
Andreas Brauneis <1274508+brownson@users.noreply.github.com>
2025-03-13 21:22:24 +0100

    WebClient v6.4.4

--- IPSViewConnect/www/flutter_bootstrap.js
+++ IPSViewConnect/www/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3687986589"
+    serviceWorkerVersion: "3111021559"
   }
 });
